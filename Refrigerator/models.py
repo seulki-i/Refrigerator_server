273dcbf4bafd8b2433326ae35e7188c13b2d7c4c
seulkikim <13skk@solupia.co.kr>
2020-08-31 11:30:37 +0900

    열어줘 냉장고 서버

---
+++ Refrigerator/models.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+FOOD_TYPES = [('육류','육류'),('어류','어류'),('과일류','과일류'),('야채류','야채류'),('유제품류','유제품류'),('음료류','음료류'),('소스류','소스류'),]
+FOOD_LOCATION = [('냉장','냉장'),('냉동','냉동'),('실온','실온'),]
+
+
+
+
+# Create your models here.
+class Food(models.Model):
+    food_type = models.CharField(max_length=10)
+    food_number = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    food_name = models.CharField(max_length=15)
+    food_purchase = models.CharField(max_length=15)
+    food_exdate = models.CharField(max_length=15)
+    food_location = models.CharField(max_length=10)
+    food_image = models.CharField(max_length=30, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modify_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.food_name
+
+class Shopping(models.Model):
+    shopping_id =  models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    food_type = models.CharField(max_length=5)
+    shopping_name = models.CharField(max_length=15)
+    shopping_memo = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modify_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.shopping_name
+
+
+class Recipe(models.Model):
+    image = models.URLField()
+    link = models.URLField()
+    title = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Info(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False)
+    title = models.CharField(max_length=30)
+    post = models.TextField()
+    image = models.ImageField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
+
+class Memo(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False)
+    author = models.CharField(max_length=20, null=True)
+    memo = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modify_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.title
+
+
+
---
+++ Refrigerator/serializers.py
@@ -0,0 +1,32 @@
+
+# -*- coding: utf-8 -*-
+
+from rest_framework import serializers
+from .models import Food, Shopping, Info, Recipe, Memo
+
+
+class FoodSerializer(serializers.HyperlinkedModelSerializer):
+  
+    class Meta:
+        model = Food
+        fields=('url','food_number','food_type', 'food_name', 'food_purchase', 'food_exdate', 'food_location', 'food_image')
+
+class ShoppingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Shopping
+        fields=('url','shopping_id','food_type', 'shopping_name', 'shopping_memo')
+
+class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Recipe
+        fields=('image','link','title')
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+        class Meta:
+            model = Info
+            fields = ('id','title', 'post', 'image')
+
+class MemoSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+	    model = Memo
+	    fields = ('id','author','memo')
